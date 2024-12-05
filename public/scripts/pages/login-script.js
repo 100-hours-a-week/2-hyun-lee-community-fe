265e2baae09f6580489eaf67b26599da13131746
hyun.lee <qwe7105c@naver.com>
2024-12-05 11:18:36 +0900

    로그인 수정

--- public/scripts/pages/login-script.js
+++ public/scripts/pages/login-script.js
@@ -2,39 +2,24 @@ import { login } from "../api/api.js";
 import { validateEmail, validatePassword } from "../utils/validators.js";
 
 
-let isEmailValid = false;
-let isPasswordValid = false;
 
+let isPasswordValid = false;
+let isEmailValid = false;
 
 document.getElementById('useremail').addEventListener('input',async (e)=>{
     const email =e.target.value;
     const emailHelper =document.getElementById('emailHelper');
-    isEmailValid =validateEmail(email,emailHelper);
-  
-
     const password = document.getElementById('password').value;
     const passwordHelper = document.getElementById('passwordHelper');
-
-    isPasswordValid = validatePassword(password,passwordHelper);
-    
-    if (isEmailValid && isPasswordValid) {
-        try {
-            const result = await login(email, password);
-            if (result.success) {
-                updateLoginButton(true);
-            } else {
-                passwordHelper.textContent = "*비밀번호가 다릅니다.";
-                passwordHelper.style.visibility = "visible";
-                updateLoginButton(false);
-            }
-        } catch (error) {
-            console.error('로그인 요청 중 오류 발생:', error);
-            alert('로그인 중 오류가 발생하였습니다. 다시 시도해주세요.');
-            updateLoginButton(false);
-        }
-    } else {
+    isEmailValid = validateEmail(email,emailHelper);
+    isPasswordValid =validatePassword(password,passwordHelper);
+    if(isPasswordValid && isEmailValid){
+        updateLoginButton(true);
+    } else{
         updateLoginButton(false);
     }
+  
+
 })
 
 document.getElementById('password').addEventListener('input', handlePasswordInput);
@@ -48,7 +33,28 @@ document.getElementById('registerBtn').addEventListener('click', () => {
 
 document.getElementById('login').addEventListener('submit', async(e) => {
     e.preventDefault();
-    window.location.href='board';
+    const email = document.getElementById('useremail').value;
+    const password = document.getElementById('password').value;
+ 
+    if(isPasswordValid && isEmailValid){
+        try {
+            const result = await login(email, password);
+    
+            if (result.message ==="*비밀번호가 다릅니다.") {
+                passwordHelper.textContent = "*비밀번호가 다릅니다.";
+                passwordHelper.style.visibility = "visible";
+            } else if(result.message ==="*이메일이 존재하지않습니다."){
+                emailHelper.textContent ="*이메일이 존재하지않습니다.";
+                emailHelper.style.visibility = "visible";
+            } else{
+                window.location.href='board';
+            }
+        } catch (error) {
+            console.error('로그인 요청 중 오류 발생:', error);
+            alert('로그인 중 오류가 발생하였습니다. 다시 시도해주세요.');
+         }
+    } 
+    
 });
 
 
@@ -60,27 +66,13 @@ async function handlePasswordInput(e){
     const passwordHelper = document.getElementById('passwordHelper');
 
     isPasswordValid = validatePassword(password, passwordHelper);
-
-
+    isEmailValid = validateEmail(email,emailHelper);
     if(isPasswordValid && isEmailValid){
-    try {
-        const result = await login(email, password);
-
-        if (result.success) {
-            updateLoginButton(true);
-        } else {
-            passwordHelper.textContent = "*비밀번호가 다릅니다.";
-            passwordHelper.style.visibility = "visible";
-            updateLoginButton(false);
-        }
-    } catch (error) {
-        console.error('로그인 요청 중 오류 발생:', error);
-        alert('로그인 중 오류가 발생하였습니다. 다시 시도해주세요.');
-        updateLoginButton(false);
-     }
+        updateLoginButton(true);
     } else{
         updateLoginButton(false);
     }
+
 }
 
 
