2369ed3f1e5ae23f545694720219bf70a9f966de
hyun.lee <qwe7105c@naver.com>
2025-01-15 16:17:01 +0900

    test..

--- public/scripts/components/header-component.js
+++ public/scripts/components/header-component.js
@@ -7,7 +7,7 @@ export function headerComponent(userInfo,showBackButton = false){
     topContainer.innerHTML=`
      <div class="top-content">
      ${showBackButton ? `<button class="back-button" onclick="history.back()|| 'board'">&#60;</button>` : '<button class="back-button empty"></button>'}
-  <h1 class="top-title">아무말 대잔치</h1>
+  <h1 class="top-title">아무말 123 대잔치</h1>
     <img id="imagePreview" class="preview" src="${profileImageUrl}" alt="">
         <div id= "dropdownMenu"class="dropdown-menu" style="display: none;">
             <div class="menu">
